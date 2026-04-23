4b10aee8909f8ea72bd1d005c5fc0cee83899fa2
DiamaraJakelineMuñozVargas <diamaramunozvargas@gmail.com>
2026-04-23 17:14:24 -0400

    primeros cambios

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Inicio from './pages/inicio';
+import Navbar from './componentes/Navbar';
+import './App.css'
+
+const App = () => {
+  return (
+    <div >
+      <Navbar/>
+     <Inicio/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Inicio.tsx
@@ -0,0 +1,12 @@
+import Tiptap from "../componentes/Tiptap";
+
+const Inicio = () => {
+  return (
+    <div >
+      
+      <Tiptap />
+    </div>
+  )
+}
+
+export default Inicio
