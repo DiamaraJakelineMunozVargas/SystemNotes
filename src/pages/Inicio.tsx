10bdccdd3f0e74baf97efb81b98cc42226d5f367
DiamaraJakelineMuñozVargas <diamaramunozvargas@gmail.com>
2026-04-29 17:20:55 -0400

    cambios

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,11 +1,12 @@
 import ModalComponente from "../componentes/ModalComponente";
-
+import Formulario from "../componentes/Formulario";
 
 
 
 const Inicio = ({data}) => {
   return (
     <div >
+      <Formulario></Formulario>
       <ModalComponente data={data}/>
      
     </div>
